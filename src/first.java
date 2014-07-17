de2ea959f5ad753a2220d36913ef354d1f5e58a7
sreep26 <sreep26@gmail.com>
2014-07-17 18:50:39 +0530

    second commit

--- src/first.java
+++ src/first.java
@@ -4,6 +4,7 @@ public class first {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("first line");
+		System.out.println("seond line");
 
 	}
 
